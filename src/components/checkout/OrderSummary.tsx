029f32e3d065247d7ed4c12278473fa4785ab5bb
Tezeh <beta2mrw@gmail.com>
2026-04-09 16:46:57 +0530

    fetch cart

--- src/components/checkout/OrderSummary.tsx
+++ src/components/checkout/OrderSummary.tsx
@@ -72,7 +72,7 @@ export default function OrderSummary({
 
   const fetchPreview = async () => {
     try {
-      const res = await fetch('/checkout/preview', {
+      const res = await fetch('https://api.wowwpe.io/checkout/preview', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ checkoutSessionId }),
